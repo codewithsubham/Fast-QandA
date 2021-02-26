71b35e2774f2a52404aec4a7609c307a899dcded
codewithsubham <59691474+codewithsubham@users.noreply.github.com>
2021-02-26 15:41:03 +0530

    rendering question and attaching eventlistner at runtim

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,67 @@
-console.log("hello world");
+import * as renderQuestion from "./views/renderQuestion";
+import { elements } from "./views/elements";
+
+let socket;
+
+window.addEventListener("load", () => {
+    console.log("window loaded");
+    initConnection();
+    if (userType === "responder") {
+        // render for responder
+
+        return;
+    }
+
+    // render for poster
+});
+
+let initConnection = () => {
+    let st =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InN1YmhhbSIsInBhc3N3b3JkIjoiYXNkYXNkIiwiZW1haWwiOiJzdWJoYW1wcmFzYWQ1NTJAZ21haWwuY29tIiwicGhvbmUiOiI5MjA1NTQ2MTczIiwiaWF0IjoxNjE0MzIwNTA4LCJleHAiOjE2MTY5MTI1MDh9.2yi2VY_itNL-yS0m70oioB4IzFjDjUvIG1b-gQGZTXo";
+    let room = "room";
+
+    socket = io(`${window.location.hostname}:3000`, {
+        query: { room: room, userType: "admin", details: st },
+    });
+
+    socket.on("connect", () => {
+        console.log("connected");
+    });
+    socket.on("test", (data) => {
+        renderQuestion.renderQuestion(data);
+    });
+    socket.on(`${room}-timer`, (data) => {
+        console.log(data, " seconds");
+    });
+    socket.on("connect_error", (err) => {
+        console.log(err.message); // prints the message associated with the error
+    });
+};
+
+/*
+document.querySelector("#delete").addEventListener("click", () => {
+clear(document.querySelector("input[name=card]:checked").value);
+});
+
+*/
+// send answer for last question
+
+let sendAnswer = (value) => {
+    console.log(
+        document.querySelector("input[name=radio]:checked").value,
+        "formdata"
+    );
+    return;
+    document.querySelector(".container").innerHTML = `
+    <iframe
+    width="600"
+    src="https://www.youtube.com/embed/QmdZmUoM6GQ?rel=0&showinfo=0&controls=1&autoplay=true&fullscreen=0&Settings=0"
+    frameborder="0"
+></iframe>`;
+
+    // socket.emit(`${room}-receiveAnsWer`, { id: value });
+};
+
+let clear = (value) => {
+    socket.emit(`${room}-clearPreviousQuestion`, { id: value });
+};
---
+++ src/js/views/renderQuestion.js
@@ -0,0 +1,40 @@
+import { elements } from "./elements";
+
+export const renderQuestion = (data) => {
+    let element = `<div class="question_container-wrapper">
+    <div class="question_container">
+        <p>
+        ${data.question}
+                    </p>
+        <div class="timer">${data.remainingtime}</div>
+    </div>
+</div>
+
+<div class="option_container">
+    <form id="question_form">
+        
+       
+    </form>
+</div>
+<div class="button_container">
+    <button id="send" >submit</button>
+</div>`;
+
+    document.querySelector(".main_container").innerHTML = element;
+
+    for (let option in data.options) {
+        let optionsHTML = ` <div class="inputGroup">
+        <input id="${option}" name="radio" value='${option}' type="radio" />
+        <label for="${option}">${data.options[option]}</label>
+    </div>`;
+        document
+            .querySelector("#question_form")
+            .insertAdjacentHTML("beforebegin", optionsHTML);
+    }
+
+    elements.timer = document.querySelector(".timer");
+
+    document.querySelector("#send").addEventListener("click", () => {
+        console.log("hello world");
+    });
+};
